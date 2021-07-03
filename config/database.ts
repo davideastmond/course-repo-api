877c02dad10e33e05b55b92cb53fad78b2beed8c
David Eastmond <davebuck1981@gmail.com>
2021-07-03 10:15:48 -0400

    initial setup and configuration

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,10 @@
-import config from "config";
+require("dotenv").config();
+
 import { ConnectionOptions, connect } from "mongoose";
 
 const connectDB = async () => {
   try {
-    const mongoURI: string = config.get("mongoURI");
+    const mongoURI: string = process.env.DEV_MONGO_URI;
     const options: ConnectionOptions = {
       useNewUrlParser: true,
       useCreateIndex: true,
@@ -14,7 +15,6 @@ const connectDB = async () => {
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
-    // Exit process with failure
     process.exit(1);
   }
 };
