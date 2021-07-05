2bf221fabfffce274cc8d704ff96cb75b345990f
David Eastmond <davebuck1981@gmail.com>
2021-07-05 10:07:49 -0400

    removed more boilerplate

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,12 +1,7 @@
-import bcrypt from "bcryptjs";
-import config from "config";
 import { Router, Response } from "express";
-import { check, validationResult } from "express-validator/check";
-import HttpStatusCodes from "http-status-codes";
-import jwt from "jsonwebtoken";
 
-import auth, { logoutIfAuthenticated } from "../../middleware/auth";
-// import Payload from "../../types/Payload";
+import { logoutIfAuthenticated } from "../../middleware/auth";
+
 import Request from "../../types/Request";
 
 import passport from "passport";
