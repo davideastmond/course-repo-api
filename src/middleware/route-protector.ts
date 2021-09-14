42bf158557dcd06e59982f9c25f819d1ddde94c7
David Eastmond <davebuck1981@gmail.com>
2021-09-14 10:45:05 -0400

    added API token security feature to API

--- src/middleware/route-protector.ts
+++ src/middleware/route-protector.ts
@@ -10,8 +10,16 @@ export function routeProtector(req: any, res: Response, next: NextFunction) {
 }
 
 export function secureRequest(req: any, res: any, next: any) {
+  const isProduction = !(
+    process.env.NODE_ENV && process.env.NODE_ENV.match("development")
+  );
+
+  const API_TOKEN = isProduction
+    ? process.env.PRODUCTION_API_TOKEN
+    : process.env.DEV_API_TOKEN;
+
   const headerToken = req.header("API_TOKEN");
-  if (headerToken === process.env.API_TOKEN) {
+  if (headerToken === API_TOKEN) {
     next();
   } else {
     res.status(401).send({ error: "API token is missing or invalid" });
