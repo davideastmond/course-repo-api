cd048d3e76d67474073212e3d560447580e5d923
David Eastmond <davebuck1981@gmail.com>
2021-12-11 17:35:45 -0500

    began work on edx

---
+++ src/services/html-parser/providers/edx/edx.ts
@@ -0,0 +1,8 @@
+import { ILearningProvider } from "../../types";
+
+export const EdxProvider: ILearningProvider = {
+  title: (root: any) => "",
+  description: (root: any) => "",
+  keyPoints: (root: any) => [],
+  category: (root: any) => [],
+};
